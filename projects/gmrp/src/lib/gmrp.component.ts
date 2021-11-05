8add26554cb5c4124590d5f0f4747a37de405240
gmrp22 <gmrp22@hotmail.com>
2021-11-05 15:00:18 -0600

    lib

--- projects/gmrp/src/lib/gmrp.component.ts
+++ projects/gmrp/src/lib/gmrp.component.ts
@@ -1,21 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output, SimpleChanges } from '@angular/core';
 import { GmrpService } from './gmrp.service';
 import { Data } from './data';
+import { Input } from '@angular/core';
 @Component({
   selector: 'lib-gmrp',
   templateUrl: './gmrp.component.html',
   styleUrls: ['./gmrp.sass'],
 })
 export class GmrpComponent implements OnInit {
+  @Input() number: number
+  @Output() alert = new EventEmitter<any>();
   data: any;
-  constructor(private gmrp: GmrpService) {}
-  /* 
+  constructor(private gmrp: GmrpService) {
+    this.number=1
+  }
+  /*
 Genera un numero aleatorio y hace la peticion
 */
-  async ngOnInit(): Promise<void> {
-    let number = Math.floor(Math.random() * (398 - 1 + 1)) + 1;
-    await this.gmrp.getPersonaje(number).subscribe((val) => {
-      this.data = val;
-    });
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.number.currentValue != changes.number.previousValue) {
+      this.alert.emit("Cambio de personaje")
+      this.gmrp.getPersonaje(this.number).subscribe((val) => {
+        this.data = val;
+      });
+    }
   }
+  ngOnInit(): void {}
+
+ 
+    
+
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,32 @@ import { Component } from '@angular/core';
 import { GmrpComponent } from 'projects/gmrp/src/src';
 @Component({
   selector: 'app-root',
-  template:  `<lib-gmrp> </lib-gmrp>`,
-  styleUrls: ['./app.component.sass']
+  templateUrl: './app.component.html' ,
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
+  newNumber = 1;
+  flag=false
+  MSG=""
   title = 'libreriaGmrp';
+
+  ngDoCheck(): void {
+    if(this.flag){
+      this.newNumber += 1;
+      this.flag=false
+    }
+    
+  }
+  nextPersonaje(){
+    if(this.flag){
+      this.flag=false
+    }
+    else{
+      this.flag=true
+    }
+  }
+  cambio(response: any){
+    this.MSG=response
+    console.log(this.MSG)
+  }
 }
